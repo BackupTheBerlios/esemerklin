5666de4909a2dfdd37366f7fe154a35ee00b0f62
nadorwo <nadorwo>
2006-06-05 19:10:23 +0000

    In der main() wurde alles soweit durchkommentiert

--- Socket_Server/socket.c
+++ Socket_Server/socket.c
@@ -17,7 +17,7 @@
 *
 *******************************************************************************
 *
-* $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/esemerklin/Repository/Socket_Server/socket.c,v 1.1 2006/06/05 12:36:46 nadorwo Exp $
+* $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/esemerklin/Repository/Socket_Server/socket.c,v 1.2 2006/06/05 19:10:23 nadorwo Exp $
 *
 ******************************************************************************/
 
@@ -26,7 +26,7 @@
 * Includes
 ******************************************************************************/
 #include <stdio.h>
-#include <clib.h>
+#include "clib.h"
 
 /******************************************************************************
 * Constants
@@ -77,28 +77,40 @@ int main(void)
    printf("\r\nOpen socket");
    #endif
    
-   //API call open socket
+   /**********************************************************************/
+   //API call open socket START
+   /**********************************************************************/   
+   //Hiermit wird ein Socket geöffnet. D.h. nicht das eine Socketverbindung aufgebaut wird
+   //es steht jetzt nur ein Socket zur Verfügung
+   //1.Param.: SOCK_STREAM = 1 für TCP socket(Verbindungorientiert)
+   //2.Param.: Fehlercode
+   //Rückgabewert: -1==API_ERROR ansonsten Socket deskriptor
    retval = opensocket( SOCK_STREAM, &error );
 
-   if(retval == API_ERROR)
-   {
+   	if(retval == API_ERROR){
        printf("\r\nSocket open failed: %d",error);
        goto TCP_SERVER_DONE;
-   }
-   else
-   {
+	   }
+   	else{
        sd = retval;
-   }
+   		}
+   /**********************************************************************/
+   //API call open socket ENDE
+   /**********************************************************************/
+   
 
    /**********************************************************************/
    //API-Call bind
    /**********************************************************************/
-   //addr.sin_len    =  sizeof(struct sockaddr_in);
-   addr.sin_family =  PF_INET;
-   //convert your sending port to correct byte order
+   //addr.sin_len    =  sizeof(struct sockaddr_in); // Ausgeklammert von Beck
+   
+   // Weist einem Socket eine lokale Protokoll Adresse zu   
+   addr.sin_family =  PF_INET; // Protocoll Familie 
+   //convert your sending port to correct byte order! SEHR WICHTIG
    retval = htons(TM_PORT_ECHO);
-
    addr.sin_port =  retval;
+   
+   // Das 'sin_addr' Attribut ist eine einfache IP Adresse in big endian byte order
    addr.sin_addr.s_addr =  0L;
 
    //call bind
@@ -115,33 +127,46 @@ int main(void)
      /**********************************************************************/
      //API-Call listen for connections
      /**********************************************************************/
+     //Mit Listen versetzt man einen Socket in Lauschmodus
      printf("\r\nTCPserver: Listening for connection");
 
+     
+     // @Param1.: Der eigentliche Socket
+     // @Param2.: Der Backlog, hier kann man die Länge der Warteschlange eingeben für die 
+     //			  nicht vollständig verbundenen Clients. Z.b. Solche die eine Anfrage gesendet haben 
+     //			  und auf einen Response warten(Thema Parallele Clients bedienen). Laut Beck API 
+     //			  sind maximal 5 ausstehende Verbindung hier möglich.
+     //           http://www.beck-ipc.com/files/ipc/documentation/api/scxxx/CLIB/TCPIP/tcp0E.htm
+     // @Param3.: Fehlercode
+     // Rückgabewert: 0 für erfolgreich ansonsten -1=API_ERROR
      retval = listen( sd, 1, &error );
 
-     if(retval == API_ERROR)
-     {
+     	if(retval == API_ERROR){
          printf("\r\nTCPserver: Socket listen failed: %d",error);
          goto TCP_SERVER_DONE;
-     }
+	     }
 
 
 
      /**********************************************************************/
      //API-Call accept, establish a connection
      /**********************************************************************/
-
+	 // Holt einen Client ab von einem lauschendem Socket
      claddr.sin_family      =  PF_INET;
      claddr.sin_port        =  0;
      claddr.sin_addr.s_addr =  0L;
 
+	 // @Param1.: Socketdeskriptor der lauscht(MUß VORHER IN DEN LISTEN-MODUS VERSTEZT WERDEN)
+	 // @Param2.: Ein Optionaler Parameter, ein Zeiger auf eine Struktur worüber man die Client IP und
+	 //  		  Port ermitteln kann. Kann man auch mit 0 stehen lassen
+	 // @Param3.: Fehlercode
+	 // Rückgabewert: 0 für erfolgreich ansonsten -1=API_ERROR
      retval = accept( sd, (struct sockaddr *)&claddr, &error );
 
-     if(retval == API_ERROR)
-     {
+     	if(retval == API_ERROR){
          printf("\r\nTCPserver: Socket accept failed: %d",error);
          goto TCP_SERVER_DONE;
-     }
+		}
 
      //save the new socketdescriptor
      asd = retval;
@@ -152,6 +177,10 @@ int main(void)
 
      printf("\r\nTCPserver: Connected with %s , Port %u\r\n",ClientIP, htons(claddr.sin_port));
 
+     
+     
+     
+     
      while(established)   //loop forever
      {
 
@@ -162,54 +191,61 @@ int main(void)
        do
        {
 
-          //recv
-          retval = recv( asd, (char *)recvbuf, TM_TCPECHOBUF_SERVER_RECVSIZE,
-                         MSG_TIMEOUT, 20000L, &error );
+          //recv() wird eingesetzt um Daten von einem verbundenem Socket zu lesen
+          // @Param1.: Der Socket Deskriptor des Clients
+          // @Param2.: Zeigt auf einen Puffer der die aufzunehmenden Daten aufnehmen soll
+          // @Param3.: Größe des Puffers(hier eine Konstante mit dem wert 1024)
+          // @Param4.: Hier wird ein optionale Flag gesetzt. 
+		  // 		   Auszug aus der API:
+		  //          		Wait option:
+	      //    					* MSG_BLOCKING - Sleep until data comes in.
+		  //  						* MSG_TIMEOUT - The caller wakes up after timeout or if bufLen data is received.
+    	  //						* MSG_DONTWAIT - Return immediately after collecting what ever data is ready.
+    	  //     Sieh dazu API: http://www.beck-ipc.com/files/ipc/documentation/api/scxxx/CLIB/TCPIP/tcp10.htm
+		  // @Param5.: Maximalla Zeit in Millisekunden wenn flag = MSG_TIMEOUT gesetzt isz
+		  // @Param6.: Fehlercode, 
+		  // Rückgabewert: -1=API_ERROR ansonsten Anzahl der gesendeten Bytes die in den Puffer geschrieben werden
+          retval = recv( asd, (char *)recvbuf, TM_TCPECHOBUF_SERVER_RECVSIZE, MSG_TIMEOUT, 20000L, &error );
 
-          if(retval == API_ERROR)
-          {
-             #ifdef TCP_ECHO_SERVER_DEBUG
-             printf("\r\nTCPserver: Recverror %d",error);
-             #endif
-           established = 0;
-           break;
-          }
-          else
-          {
-             if( retval > 0)    //data received
-             {
-             #ifdef  PRINT_DATA
-             printf("\r\nTCPserver: Received data\r\n");
-                 for(i=0;i<retval; i++)
-                 {
-                   printf("%02X ",(char)recvbuf[i]);
-                 }//for(i=0;i<outregs.x.ax;i++)
-             printf("\r\n");
-             #endif
-             /********************************/
-             //echo data back to the client
-             /********************************/
+          	if(retval == API_ERROR){
+             	#ifdef TCP_ECHO_SERVER_DEBUG
+	            printf("\r\nTCPserver: Recverror %d",error);
+    	        #endif
+           	   established = 0;
+           	   break;
+          		}
+          	else{
+          		
+               	if( retval > 0){//data received
+                    #ifdef  PRINT_DATA
+             		printf("\r\nTCPserver: Received data\r\n");
+                 	for(i=0;i<retval; i++){
+                   		printf("%02X ",(char)recvbuf[i]);
+                 	}//for(i=0;i<outregs.x.ax;i++)
+             		printf("\r\n");
+             		#endif
+             				             				
+/*****************************Rückgabe zum Client START ******************************/
 
-               //send
-               retval = send( asd, (char *)recvbuf, retval, 0, &error );
+               		//send
+               		retval = send( asd, (char *)recvbuf, retval, 0, &error );
 
-               if(retval == API_ERROR)
-               {
-                   #ifdef TCP_ECHO_SERVER_DEBUG
-                   printf("\r\nTCPserver: Senderror %d",error);
-                   #endif
-                 established = 0;
-                 break;
-               }
+               		if(retval == API_ERROR){
+                   		#ifdef TCP_ECHO_SERVER_DEBUG
+                   	    printf("\r\nTCPserver: Senderror %d",error);
+                   		#endif
+                 		established = 0;
+                 		break;
+               			}
+			   	 }//if(retval > 0)    //data received
+           	    else{
+             		#ifdef TCP_ECHO_SERVER_DEBUG
+             		printf("\r\nTCPserver: Nothing received %d\r\n",error);
+                	#endif
+           			}
+          		}//elseif(retval == API_ERROR)
 
-             }//if(retval > 0)    //data received
-           else
-           {
-             #ifdef TCP_ECHO_SERVER_DEBUG
-             printf("\r\nTCPserver: Nothing received %d\r\n",error);
-             #endif
-           }
-          }//elseif(retval == API_ERROR)
+/*****************************Rückgabe zum Client ENDE ********************************/
 
 
           /*****************************************************************/
@@ -218,14 +254,11 @@ int main(void)
 
           retval = GetWaitingBytes( asd, &error );
 
-        #ifdef TCP_ECHO_SERVER_DEBUG
-          if(retval==API_ENOERROR)
-          {
-
-            printf("\r\nTCPserver: Bytes waiting on the socket  %d",retval);
-
-          }
-        #endif
+        	#ifdef TCP_ECHO_SERVER_DEBUG
+          	if(retval==API_ENOERROR){
+	           printf("\r\nTCPserver: Bytes waiting on the socket  %d",retval);
+	         }
+        	#endif
        }//while data available
        while((retval!=API_ERROR) && (retval>0));
      }//while(established)
@@ -235,13 +268,12 @@ int main(void)
      printf("\r\nTCPserver: Closing connection");
 
      retval = closesocket( asd, &error );
-
-     #ifdef TCP_ECHO_SERVER_DEBUG
-     if(retval==API_ERROR)
-     {
-      printf("\r\nTCPserver: Socket close failed: %d",error);
-     }
-     #endif
+	
+	     #ifdef TCP_ECHO_SERVER_DEBUG
+    	 if(retval==API_ERROR){
+		      printf("\r\nTCPserver: Socket close failed: %d",error);
+     		}
+     	#endif
 
    }//while(1)
 
